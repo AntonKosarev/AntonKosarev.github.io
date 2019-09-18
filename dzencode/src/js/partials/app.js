28a9427cce25807ffa64016dcd6363c745d1cb9f
Anton <rasto4itel@gmsil.com>
2019-09-18 15:19:08 +0300

    fonts

--- dzencode/src/js/partials/app.js
+++ dzencode/src/js/partials/app.js
@@ -16,61 +16,25 @@ $(document).ready(function() {
   //Плавный скролл по странице
   $('a[href*="#"]').on('click', function(e) {
     e.preventDefault();
-
     $('html, body').animate({
       scrollTop: $($(this).attr('href')).offset().top
     }, 500, 'linear');
   });
+
   //динамически добавляю блоки в слайдерe
+  var slic_slider = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    cssEase: 'linear',
+    arrows: false,
+    customPaging: function(slider, i) {
+        return $('<span></span>');
+        return $('<button type="button" />').text(i + 1);
+    },
+    vertical: true,
+    verticalSwiping: true,
+  };
+  $('.comment_slider').slick(slic_slider);
 
-
-
-  var comment_slider = [
-    '#comment_slider_1',
-    '#comment_slider_2',
-    '#comment_slider_3'
-  ];
-  for (var j = 0; j < comment_slider.length; j++) {
-    for (var i = 0; i < 3; i++) {
-      $(".comment_item").clone().appendTo(comment_slider[j]).removeClass('comment_item');
-    }
-    /*$(comment_slider[j]).bxSlider({
-      pager: true,
-      mode: 'vertical',
-      nextText: '<span>nextText</span>',
-      prevText: '<span>nextText</span>',
-    });*/
-
-  }
-  $("#comment_slider_1").bxSlider({
-    mode: 'vertical',
-    pager: true,
-    nextText: null,
-    prevText: null,
-    wrapperClass: 'bx-wrapper_inner',
-    slideMargin: 100,
-    myViewportHeight: 247,
-
-  });
-  $("#comment_slider_2").bxSlider({
-    mode: 'vertical',
-    pager: true,
-    nextText: null,
-    prevText: null,
-    wrapperClass: 'bx-wrapper_inner',
-    slideMargin: 100,
-    myViewportHeight: 247,
-
-
-  });
-  $("#comment_slider_3").bxSlider({
-    mode: 'vertical',
-    pager: true,
-    nextText: null,
-    prevText: null,
-    wrapperClass: 'bx-wrapper_inner',
-    slideMargin: 100,
-    myViewportHeight: 247,
-
-  });
 });
