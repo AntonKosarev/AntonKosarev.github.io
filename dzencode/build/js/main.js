d6e4b7ce5d569af8cae3d1c846c6f2c1017699bc
Anton <rasto4itel@gmsil.com>
2019-09-18 16:58:20 +0300

    осталась кросбраузернасть на осле и мобильное отображение

--- dzencode/build/js/main.js
+++ dzencode/build/js/main.js
@@ -15309,12 +15309,59 @@ $(document).ready(function() {
     cssEase: 'linear',
     arrows: false,
     customPaging: function(slider, i) {
-        return $('<span></span>');
-        return $('<button type="button" />').text(i + 1);
+      return $('<span></span>');
+      return $('<button type="button" />').text(i + 1);
     },
     vertical: true,
     verticalSwiping: true,
   };
   $('.comment_slider').slick(slic_slider);
+
+  //form validation
+  var validName = false;
+  var validPhone = false;
+  var validClass = false;
+
+  $(".form_try-it form").submit(function(event) {
+    event.preventDefault();
+
+    var name = $('input[name = "name"]');
+    var phone = $('input[name = "phone"]');
+    var value_class = $('input[name = "class"]');
+    var err_css =  'box-shadow: 0px 0px 3px 2px #FF0000;';
+    var suc_css =  'box-shadow: none';
+
+    if (name.val() == "" || name.val() == 'Enter your name') {
+      name.parent().attr('style', err_css);
+      validName = false;
+    } else {
+      name.parent().attr('style', suc_css);
+      validName = true;
+    }
+
+    if (phone.val() == "" || phone.val() == "Enter your phone") {
+      phone.parent().attr('style', err_css);
+      validPhone = false;
+    } else {
+      phone.parent().attr('style', suc_css);
+      validPhone = true;
+    }
+
+    if (value_class.val() == "" || value_class.val() == "Chose your class") {
+      value_class.parent().attr('style', err_css);
+      validClass = false;
+    } else {
+      value_class.parent().attr('style', suc_css);
+      validClass = true;
+    }
+
+    if (validName == true && validPhone == true) {
+      if (validClass == true) {
+        $(".form_try-it").unbind('submit').submit();
+      }
+    }
+
+  });
+
 
 });
